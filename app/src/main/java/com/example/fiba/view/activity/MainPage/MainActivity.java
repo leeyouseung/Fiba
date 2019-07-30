fe17dbedbb2caa9f7530e7dfc8dd6d9fa78cf721
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-07-30 13:58:06 +0900

    fragment set

--- app/src/main/java/com/example/fiba/view/activity/MainPage/MainActivity.java
+++ app/src/main/java/com/example/fiba/view/activity/MainPage/MainActivity.java
@@ -2,11 +2,19 @@ package com.example.fiba.view.activity.MainPage;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.fiba.R;
 import com.example.fiba.databinding.ActivityMainBinding;
+import com.example.fiba.view.fragment.AddFragment;
+import com.example.fiba.view.fragment.MainFragment;
+import com.example.fiba.view.fragment.MyPageFragment;
+import com.example.fiba.view.fragment.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +41,97 @@ public class MainActivity extends AppCompatActivity {
 
     private void clickEvent() {
 
+        clickMainButton();
+        clickAddButton();
+        clickSearchButton();
+        clickMyPageButton();
+    }
+
+    private void clickMainButton() {
+
+        binding.mainButton.setOnClickListener(v -> {
+
+            binding.mainButton.setImageDrawable(getResources().getDrawable(R.drawable.list_act));
+            binding.addButton.setImageDrawable(getResources().getDrawable(R.drawable.plus));
+            binding.searchButton.setImageDrawable(getResources().getDrawable(R.drawable.search));
+            binding.myPageButton.setImageDrawable(getResources().getDrawable(R.drawable.people));
+
+            binding.mainButtonText.setVisibility(View.VISIBLE);
+            binding.addButtonText.setVisibility(View.INVISIBLE);
+            binding.searchButtonText.setVisibility(View.INVISIBLE);
+            binding.myPageButtonText.setVisibility(View.INVISIBLE);
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            fragmentTransaction.replace(R.id.fragment, new MainFragment());
+            fragmentTransaction.commit();
+        });
+    }
+
+    private void clickAddButton() {
+
+        plusButton.setOnClickListener(v -> {
+
+            listButton.setImageDrawable(getResources().getDrawable(R.drawable.list));
+            plusButton.setImageDrawable(getResources().getDrawable(R.drawable.plus_act));
+            searchButton.setImageDrawable(getResources().getDrawable(R.drawable.search));
+            peopleButton.setImageDrawable(getResources().getDrawable(R.drawable.people));
+
+            listButtonText.setVisibility(View.INVISIBLE);
+            plusButtonText.setVisibility(View.VISIBLE);
+            searchButtonText.setVisibility(View.INVISIBLE);
+            peopleButtonText.setVisibility(View.INVISIBLE);
+
+            Intent intent = new Intent(getApplicationContext(), AddFragment.class);
+            startActivity(intent);
+        });
+    }
+
+    private void clickSearchButton() {
+
+        searchButton.setOnClickListener(v -> {
+
+            listButton.setImageDrawable(getResources().getDrawable(R.drawable.list));
+            plusButton.setImageDrawable(getResources().getDrawable(R.drawable.plus));
+            searchButton.setImageDrawable(getResources().getDrawable(R.drawable.search_act));
+            peopleButton.setImageDrawable(getResources().getDrawable(R.drawable.people));
+
+            listButtonText.setVisibility(View.INVISIBLE);
+            plusButtonText.setVisibility(View.INVISIBLE);
+            searchButtonText.setVisibility(View.VISIBLE);
+            peopleButtonText.setVisibility(View.INVISIBLE);
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            fragmentTransaction.replace(R.id.fragmentMain, new SearchFragment());
+            fragmentTransaction.commit();
+        });
+    }
+
+    private void clickMyPageButton() {
+
+        peopleButton.setOnClickListener(v -> {
+
+            listButton.setImageDrawable(getResources().getDrawable(R.drawable.list));
+            plusButton.setImageDrawable(getResources().getDrawable(R.drawable.plus));
+            searchButton.setImageDrawable(getResources().getDrawable(R.drawable.search));
+            peopleButton.setImageDrawable(getResources().getDrawable(R.drawable.people_act));
+
+            listButtonText.setVisibility(View.INVISIBLE);
+            plusButtonText.setVisibility(View.INVISIBLE);
+            searchButtonText.setVisibility(View.INVISIBLE);
+            peopleButtonText.setVisibility(View.VISIBLE);
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            fragmentTransaction.replace(R.id.fragmentMain, new MyPageFragment());
+            fragmentTransaction.commit();
+        });
     }
 }
