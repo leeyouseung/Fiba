45da51f52391c104f0e9317ad5eaa17a6c7584a9
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-21 16:04:35 +0900

    Update FindChildDatabase.java

--- app/src/main/java/com/example/fiba/database/table/FindChildDatabase.java
+++ app/src/main/java/com/example/fiba/database/table/FindChildDatabase.java
@@ -1,6 +1,61 @@
 package com.example.fiba.database.table;
 
-import androidx.room.RoomDatabase;
+import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.example.fiba.database.db_api.FindChildDao;
+import com.example.fiba.model.FindChild;
+
+@Database(entities = {FindChild.class}, version = 1)
 public abstract class FindChildDatabase extends RoomDatabase {
+
+    private static FindChildDatabase sInstance;
+
+    public abstract FindChildDao findChildDao();
+
+    public static synchronized FindChildDatabase getInstance(Context context) {
+
+        if(sInstance == null) {
+
+            sInstance = Room.databaseBuilder(context.getApplicationContext(), FindChildDatabase.class, "fiba_database")
+                    .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
+                    .build();
+        }
+
+        return sInstance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+            new PopulateDbAsyncTask(sInstance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        private FindChildDao findChildDao;
+
+        private PopulateDbAsyncTask(FindChildDatabase db) {
+
+            findChildDao = db.findChildDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            findChildDao.insert(new FindChild());
+
+            return null;
+        }
+    }
 }
