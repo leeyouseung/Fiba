302a9533b53bfa8a29b379fcfb00f067069930d2
leeyouseung <38912467+leeyouseung@users.noreply.github.com>
2019-08-21 15:59:10 +0900

    Update FindChildDao.java

--- app/src/main/java/com/example/fiba/database/db_api/FindChildDao.java
+++ app/src/main/java/com/example/fiba/database/db_api/FindChildDao.java
@@ -1,13 +1,31 @@
 package com.example.fiba.database.db_api;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
 
 import com.example.fiba.model.FindChild;
+
+import java.util.List;
 
 @Dao
 public interface FindChildDao {
 
     @Insert
     void insert(FindChild findChild);
+
+    @Update
+    void update(FindChild findChild);
+
+    @Delete
+    void delete(FindChild findChild);
+
+    @Query("DELETE FROM find_child_table")
+    void deleteAllFindChild();
+
+    @Query("SELECT * FROM find_child_table ORDER BY childId DESC")
+    LiveData<List<FindChild>> getAllFindChild();
 }
